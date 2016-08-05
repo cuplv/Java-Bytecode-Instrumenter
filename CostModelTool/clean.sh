81d95489655d1f38c03f20e840bf1851d861fa76
Aniket Kumar Lata <aniket.lata@colorado.edu>
2016-08-05 16:49:08 -0600

    Cost model deleted

--- CostModelTool/clean.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd MethodCountInstrument
-rm -rf *.zip log *.txt
-cd ../ExecTime/instrument
-rm -rf *.zip $1 Duration.txt Duration_updated.txt
-cd ../../PostProcessing
-rm -rf Summary.CSV Rplots.pdf
-cd ..
